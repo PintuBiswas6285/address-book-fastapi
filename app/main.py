0296c4c7884c6bcc7df90f45e6e07109cd5a5a09
PintuBiswas6285 <pk.pintoo933@gmail.com>
2026-03-27 08:19:19 +0530

    Fixed login bug and updated UI

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 # pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
 import os
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from app.database import Base, engine
 from app.api.v1.routes import router
@@ -12,13 +13,18 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Address Book API")
 
 # Jinja templates setup
-#templates = Jinja2Templates(directory="app/templates")
+templates = Jinja2Templates(directory="app/templates")
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-templates = Jinja2Templates(
-    directory=os.path.join(BASE_DIR, "templates"))
 
 # Include API routes
 app.include_router(router)
 
+# Home page (basic UI)
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    # You don't actually need to define a context dict if it only contains the request
+    return templates.TemplateResponse(
+        request=request, 
+        name="index.html", 
+        context={}  # Add other variables here if needed
+    )
